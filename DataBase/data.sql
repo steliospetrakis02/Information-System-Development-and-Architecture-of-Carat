c38d7066c74e0baf97e43bf77d09c61852a9be06
Stelios Petrakis <t8200139@aueb.gr>
2022-10-21 22:19:00 +0300

    data , er(not completed)

--- DataBase/data.sql
+++ DataBase/data.sql
@@ -3,8 +3,11 @@ use ismgroup60;
 Insert INTO User_(email,passwrd,sex,elname,efname) 
 			Values('admin@gmail.com',123456,'male','admin','-');
             
+Insert INTO User_(email,passwrd,sex,elname,efname) 
+			Values('Vodafone@gmail.com',1234567,'male','Vodafone','-');
+            
 Insert INTO Client_(campaign,Company_name,email)
-			Values('Spring','Vodafone','admin@gmail.com');
+			Values('Spring','Vodafone','Vodafone@gmail.com');
 
 Insert INTO employee(employee_id,email) 
-			Values('12345','Vodafone@gmail.com');
+			Values('12345','admin@gmail.com');
