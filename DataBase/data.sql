9de35cdd60ad31b8c49f4cb1321df2f119d2ccb0
sergiloupa <93870667+sergiloupa@users.noreply.github.com>
2022-12-13 18:03:02 +0200

    Update data.sql

--- DataBase/data.sql
+++ DataBase/data.sql
@@ -24,3 +24,12 @@ insert into indicators(ind1,ind2,ind3,ind4,reports_id)
 
 Insert into reports (reports_id,email)
 	Values("1","sts@gmail.com");
+	
+	
+INSERT INTO User_ (email,password,personal_name) VALUES ('t8200141@intel.com','123','Sergi Poula');
+INSERT INTO User_ (email,password,personal_name) VALUES ('t8200141@intel.com','321','Giorgos Mala');
+INSERT INTO User_ (email,password,personal_name) VALUES ('t8200141@intel.com','111', 'Dimitris Grammatikakhs');
+INSERT INTO User_ (email,password,personal_name) VALUES ('t8200141@intel.com','222', 'Marsid Mali');
+INSERT INTO User_ (email,password,personal_name) VALUES ('t8200141@intel.com','321','Stelios Petrakhs');
+INSERT INTO User_ (email,password,personal_name) VALUES ('AEGEAN@hotmail.com','999','Theodoros Vasilakhs');
+INSERT INTO User_ (email,password,personal_name) VALUES ('COCA-COLA@hotmail.com','321','Asa Griks Kandler');
