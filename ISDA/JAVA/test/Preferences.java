9d5435ecb1a537442006a2e9e21f084af306e45d
Stelios Petrakis <t8200139@aueb.gr>
2022-12-13 10:17:06 +0200

    /

--- ISDA/JAVA/test/Preferences.java
+++ ISDA/JAVA/test/Preferences.java
@@ -9,11 +9,11 @@ public class Preferences {
     
     //private final String userQuery = "select username from users where username =?  and password =?";
     
-    private Statement stmt1 = null;
+    PreparedStatement pre = null;
     private Connection con = null;
 
     private ResultSet rs = null, rs1 = null;
-    private final static String staffQuery = "select * from Preferences";
+   // private final static String staffQuery = "select * from Preferences";
 
     private String reports[];
     private String statistics[];
@@ -21,33 +21,32 @@ public class Preferences {
 
  
 
-    public List<String> get_Client_Preferences() throws SQLException{
+    public List<String> get_Client_Preferences(String email) throws SQLException{
         Data_connection dc = new Data_connection();
         con = dc.get_connection();
-        stmt1 = con.createStatement();
-        rs1 = stmt1.executeQuery(staffQuery);
-        while (rs1.next()){
+        String sql2 = "Select indicator1,indicator2,indicator3,indicator4 from Preferences where email=?";
+        pre = con.prepareStatement(sql2);
+        pre.setString(1, email);
+        rs = pre.executeQuery();
 
-          String email = rs1.getString("email");
-          String ind1 = rs1.getString("indicator1");
-          String ind2 = rs1.getString("indicator1");
-          String ind3 = rs1.getString("indicator1");
-          String ind4 = rs1.getString("indicator1");
-          String ind5 = rs1.getString("indicator1");
-          String stat_indicator1 = rs1.getString("stat_indicator1");
-          String stat_indicator2 = rs1.getString("stat_indicator2");
-          String company_name = rs1.getString("Company_name");
+        while (rs.next()){
+
+         // String email = rs1.getString("email");
+          String ind1 = rs.getString("indicator1");
+          String ind2 = rs.getString("indicator2");
+          String ind3 = rs.getString("indicator3");
+          String ind4 = rs.getString("indicator4");
+         // String stat_indicator1 = rs1.getString("stat_indicator1");
+         // String stat_indicator2 = rs1.getString("stat_indicator2");
 
 
-          client_pref_res.add(email);
+         // client_pref_res.add(email);
           client_pref_res.add(ind1);
           client_pref_res.add(ind2);
           client_pref_res.add(ind3);
           client_pref_res.add(ind4);
-          client_pref_res.add(ind5);
-          client_pref_res.add(stat_indicator1);
-          client_pref_res.add(stat_indicator2);
-          client_pref_res.add(company_name);
+        //  client_pref_res.add(stat_indicator1);
+         // client_pref_res.add(stat_indicator2);
         
         } 
 
@@ -87,8 +86,8 @@ public class Preferences {
         
 
         Preferences pref = new Preferences();
-        pref.set_Client_Preferences("vodafone@gmail","12","1","1","1","1","1222","Vodafone","Vodafone@gmail.com", null);
-        System.out.println(pref.get_Client_Preferences());
+        //pref.set_Client_Preferences("vodafone@gmail","12","1","1","1","1","1222","Vodafone","Vodafone@gmail.com", null);
+        System.out.println(pref.get_Client_Preferences("sts@gmail.com"));
     }
 
     }
