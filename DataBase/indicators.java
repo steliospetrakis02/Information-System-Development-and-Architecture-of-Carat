73f85b52e22b2b4c5b32622f88504f3174bb1d90
sergiloupa <93870667+sergiloupa@users.noreply.github.com>
2022-12-18 02:50:09 +0200

    Update indicators.java

--- DataBase/indicators.java
+++ DataBase/indicators.java
@@ -7,10 +7,9 @@ public class indicators {
             con = obj.getConnection();
 			Statement stmt=con.createStatement();
 			indicators a =new indicators();
-			String querry = "SELECT reports_id"
-						+ "FROM reports";
+			String querry = "SELECT reports_id FROM reports";
 			ResultSet rs = stmt.executeQuery(querry);
-			String weeks;
+			int clicks;
 			int ins;
 			double SOV;
 			double v;
@@ -18,16 +17,17 @@ public class indicators {
 			int i;
 			while(rs.next()) {
 				for(i=0; i<16; i++) {
-					weeks = a.Weeks_4x();
+					clicks = a.Clicks();
 					ins = a.Insertions();
 					SOV = a.SOV();
 					v = a.Viewability();
 					reports_id = rs.getString("reports_id");
-                    PreparedStatement pstmt = con.prepareStatement("INSERT INTO `indicators` (ind1,ind2,ind3,ind4) VALUES (?,?,?,?)");
-                    pstmt.setString(1,weeks);
+                    PreparedStatement pstmt = con.prepareStatement("INSERT INTO `indicators` (ind1,ind2,ind3,ind4,reports_id) VALUES (?,?,?,?,?)");
+                    pstmt.setInt(1,clicks);
                     pstmt.setInt(2, ins);
                     pstmt.setDouble(3,SOV);
                     pstmt.setDouble(4, v);
+					pstmt.setString(5,reports_id);
                     pstmt.executeUpdate();
 				}
 			}
@@ -36,17 +36,15 @@ public class indicators {
 		}
 	}
 
-    public String Weeks_4x() {
-        int min = 1;
-        int max = 4;
-        Integer x =(int) Math.random()*(max-min+1)+min;
-        String weeks = x.toString();
-        weeks = weeks + "x";
-        return weeks;
-    }
+public int Clicks() {
+    int min = 0;
+    int max = 200;
+    int clicks =(int) (Math.random()*(max-min+1)+min);
+    return clicks;
+}
 public int Insertions() {
 	int min= 0;
-	int max= 100;
+	int max= 300;
 	int ins = (int)(Math.random()*(max-min+1)+min);
 	return ins;
 }
@@ -54,12 +52,14 @@ public double SOV() {
 	int min= 0;
 	int max= 100;
 	double SOV = Math.random()*(max-min+1)+min;
+	SOV =  Math.floor(SOV * 100) / 100;
 	return SOV;
 }
 public double Viewability() {
 	int min = 0;
 	int max = 100;
 	double v = Math.random()*(max-min+1)+min;
+	v =  Math.floor(v * 100) / 100;
 	return v;
 }
 }
