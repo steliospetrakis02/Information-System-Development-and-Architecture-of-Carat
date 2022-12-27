cb7765ac14a9678a6c1fb827ae72cf69905de6da
sergiloupa <93870667+sergiloupa@users.noreply.github.com>
2022-12-27 12:30:12 +0200

    Update indicators.java

--- DataBase/indicators.java
+++ DataBase/indicators.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Random;
 public class indicators {
 	public static void main(String[] args) {
 		try {
@@ -9,25 +10,50 @@ public class indicators {
 			indicators a =new indicators();
 			String querry = "SELECT reports_id FROM reports";
 			ResultSet rs = stmt.executeQuery(querry);
-			int clicks;
-			int ins;
+			int GRPs;
+			double Reach_1;
+			double Reach_3;
 			double SOV;
+			int Insertions;
+			int GRPs_Week;
+			int Weeks;
+			String Weeks_4x;
+			int Impressions;
+			int clicks;
+			double click_rate;
+			double Viewability;
 			double v;
 			String reports_id;
 			int i;
 			while(rs.next()) {
 				for(i=0; i<16; i++) {
-					clicks = a.Clicks();
-					ins = a.Insertions();
+					GRPs = a.Grps();
+					Reach_1 = a.Reach_1();
+					Reach_3 = a.Reach_3();
 					SOV = a.SOV();
-					v = a.Viewability();
+					Insertions = a.Insertions();
+					GRPs_Week = a.GRPs_Week();
+					Weeks = a.Weeks();
+					Weeks_4x = a.Weeks_4x();
+					Impressions = a.Impressions();
+					clicks = a.Clicks();
+					click_rate = a.Click_Rate();
+					Viewability = a.Viewability();
 					reports_id = rs.getString("reports_id");
-                    PreparedStatement pstmt = con.prepareStatement("INSERT INTO `indicators` (ind1,ind2,ind3,ind4,reports_id) VALUES (?,?,?,?,?)");
-                    pstmt.setInt(1,clicks);
-                    pstmt.setInt(2, ins);
-                    pstmt.setDouble(3,SOV);
-                    pstmt.setDouble(4, v);
-					pstmt.setString(5,reports_id);
+                    PreparedStatement pstmt = con.prepareStatement("INSERT INTO `indicators` (GRPs,Reach_1,Reach_3,SOV,Insertions,GRPs_Week,Weeks,Weeks_4x,Impressions,clicks,	click_rate,Viewability,reports_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+                    pstmt.setInt(1,GRPs);
+                    pstmt.setDouble(2, Reach_1);
+                    pstmt.setDouble(3,Reach_3);
+                    pstmt.setDouble(4, SOV);
+					pstmt.setInt(5,Insertions);
+					pstmt.setInt(6,GRPs_Week);
+					pstmt.setInt(7,Weeks);
+					pstmt.setString(8,Weeks_4x);
+					pstmt.setInt(9,Impressions);
+					pstmt.setInt(10,clicks);
+					pstmt.setDouble(11,click_rate);
+					pstmt.setDouble(12,Viewability);
+					pstmt.setString(13,reports_id);
                     pstmt.executeUpdate();
 				}
 			}
@@ -35,18 +61,25 @@ public class indicators {
 			System.out.print("Could not insert values to table reports" + e.getMessage());
 		}
 	}
-
-public int Clicks() {
-    int min = 0;
-    int max = 200;
-    int clicks =(int) (Math.random()*(max-min+1)+min);
-    return clicks;
+public int Grps(){
+	int min = 0;
+    int max = 300;
+    int Grps =(int) (Math.random()*(max-min+1)+min);
+	return Grps;
 }
-public int Insertions() {
+public double Reach_1(){
 	int min= 0;
-	int max= 300;
-	int ins = (int)(Math.random()*(max-min+1)+min);
-	return ins;
+	int max= 100;
+	double Reach_1 = Math.random()*(max-min+1)+min;
+	Reach_1 =  Math.floor(Reach_1 * 100) / 100;
+	return Reach_1;
+}
+public double Reach_3(){
+	int min= 0;
+	int max= 100;
+	double Reach_3 = Math.random()*(max-min+1)+min;
+	Reach_3 =  Math.floor(Reach_3 * 100) / 100;
+	return Reach_3;
 }
 public double SOV() {
 	int min= 0;
@@ -54,6 +87,56 @@ public double SOV() {
 	double SOV = Math.random()*(max-min+1)+min;
 	SOV =  Math.floor(SOV * 100) / 100;
 	return SOV;
+}
+public int Insertions() {
+	int min= 0;
+	int max= 300;
+	int ins = (int)(Math.random()*(max-min+1)+min);
+	return ins;
+}
+public int GRPs_Week(){
+	int min = 0;
+    int max = 50;
+    int Grps =(int) (Math.random()*(max-min+1)+min);
+	return Grps;
+}
+public int Weeks(){
+	int min = 1;
+    int max = 4;
+    int Weeks =(int) (Math.random()*(max-min+1)+min);
+	return Weeks;
+}
+public static String Weeks_4x(){
+	Random randomNum = new Random();
+	String Weeks_4x= "";
+	for (int j=0; j<4; j++){
+		int showMe = randomNum. nextInt(100);
+		if (showMe>60) {
+			Weeks_4x = Weeks_4x + "-";
+		}else{
+			Weeks_4x= Weeks_4x + "X";
+		}
+	}
+	return Weeks_4x;
+}
+public int Impressions(){
+	int min = 0;
+    int max = 300;
+    int imp =(int) (Math.random()*(max-min+1)+min);
+	return imp;
+}
+public int Clicks() {
+    int min = 0;
+    int max = 300;
+    int clicks =(int) (Math.random()*(max-min+1)+min);
+    return clicks;
+}
+public double Click_Rate() {
+	int min = 0;
+	int max = 100;
+	double click_rate = Math.random()*(max-min+1)+min;
+	click_rate =  Math.floor(click_rate * 100) / 100;
+	return click_rate;
 }
 public double Viewability() {
 	int min = 0;
